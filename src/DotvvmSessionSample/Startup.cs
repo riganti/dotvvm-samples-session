4e7efffcde3fb67ded1468208d31a096f8bfbed7
Tomáš Herceg <tomas.herceg@riganti.cz>
2018-04-19 15:09:28 +0200

    Initial version added

---
+++ src/DotvvmSessionSample/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.SessionState;
+using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using DotVVM.Framework;
+using DotVVM.Framework.Configuration;
+using DotVVM.Framework.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Owin.Extensions;
+using Microsoft.Owin.Infrastructure;
+
+[assembly: OwinStartup(typeof(DotvvmSessionSample.Startup))]
+namespace DotvvmSessionSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use((context, next) =>
+            {
+                var httpContext = context.Get<HttpContextBase>(typeof(HttpContextBase).FullName);
+                httpContext.SetSessionStateBehavior(SessionStateBehavior.Required);
+                return next();
+            });
+            app.UseStageMarker(PipelineStage.MapHandler);
+
+
+            var applicationPhysicalPath = HostingEnvironment.ApplicationPhysicalPath;
+
+            ConfigureAuth(app);
+
+            // use DotVVM
+            var dotvvmConfiguration = app.UseDotVVM<DotvvmStartup>(applicationPhysicalPath, debug: IsInDebugMode(), options: options =>
+            {
+                options.AddDefaultTempStorages("temp");
+
+                options.Services.AddSingleton<ICookieManager, SystemWebCookieManager>();
+            });
+
+
+            // use static files
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileSystem = new PhysicalFileSystem(applicationPhysicalPath)
+            });
+        }
+        public void ConfigureAuth(IAppBuilder app)
+        {
+        }
+
+		private bool IsInDebugMode()
+        {
+#if !DEBUG
+			return false;
+#endif
+            return true;
+        }
+    }
+}
---
+++ src/DotvvmSessionSample/ViewModels/DefaultViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Hosting;
+
+namespace DotvvmSessionSample.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel
+    {
+
+
+        public string Value { get; set; }
+
+        public void Write()
+        {
+            HttpContext.Current.Session["Value"] = Value;
+        }
+
+        public void Read()
+        {
+            Value = HttpContext.Current.Session["Value"] as string;
+        }
+
+    }
+}
